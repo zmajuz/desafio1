446c72fa46d252f71521f3e28b7c282574c410e1
zmajuz <juliacabral61@gmail.com>
2024-08-13 19:51:57 -0300

    adicionando arquivo desafio1

---
+++ desafio1.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:"
+nome = gets.chomp
+
+print "Digite seu sobrenome:"
+sobrenome = gets.chomp
+
+print "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome} #{sobrenome}, você tem #{idade} anos."
